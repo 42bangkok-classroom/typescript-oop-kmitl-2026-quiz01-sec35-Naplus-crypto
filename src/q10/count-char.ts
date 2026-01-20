17d5e0937f4a0f61322ae36a20abed29ad1b27b1
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-20 19:25:55 +0700

    fixq10

--- src/q10/count-char.ts
+++ src/q10/count-char.ts
@@ -1,18 +1,28 @@
 const input = process.argv[2];
 
-if(!input){
-    process.exit(0);
+if (input === undefined) {
+  process.exit(0);
 }
-let letter=0;
-let digit=0;
-let other=0;
-for(let i = 0;i<input.length;i++){
-    const char = input[i];
 
-    if (char >= '0' && char <= '9') {
-    digit++;
+let letters = 0;
+let digits = 0;
+let others = 0;
+
+for (let i = 0; i < input.length; i++) {
+  const char = input[i];
+
+  if (char >= '0' && char <= '9') {
+    digits++;
+  } else if (
+    (char >= 'a' && char <= 'z') ||
+    (char >= 'A' && char <= 'Z')
+  ) {
+    letters++;
+  } else {
+    others++;
+  }
 }
-    else if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
-    letter++;
-}
-} 
+
+console.log(`Letters: ${letters}`);
+console.log(`Digits: ${digits}`);
+console.log(`Others: ${others}`);
