592cbdfa5c709bb1203a609edc3f0394f17d8cc1
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-20 18:48:55 +0700

    fix q05

--- src/q05/foobar.ts
+++ src/q05/foobar.ts
@@ -1,18 +1,25 @@
-const n: number = parseFloat(process.argv[2]);
-if(isNaN(n)|| n < 1 ||!Number.isInteger(n)){
-    console.log("Invalid Input");
+const arg = process.argv[2];
+
+if (arg === undefined) {
+  console.log("Invalid input");
+  process.exit(0);
 }
-else{
-    for(let i = 1;i<=n;i++){
-        if(i % 3 ===0 && i % 7 ===0){
-            console.log("FooBar");
-        } else if(i % 3 === 0){
-            console.log('Foo');
-        }else if(i % 7 === 0){
-            console.log('Bar');
-        }
-        else{
-            console.log(i);
-        }
-        }
-    }
+
+const n = Number(arg);
+
+if (!Number.isInteger(n) || n < 1) {
+  console.log("Invalid input");
+  process.exit(0);
+}
+
+for (let i = 1; i <= n; i++) {
+  if (i % 3 === 0 && i % 7 === 0) {
+    console.log("FooBar");
+  } else if (i % 3 === 0) {
+    console.log("Foo");
+  } else if (i % 7 === 0) {
+    console.log("Bar");
+  } else {
+    console.log(i.toString());
+  }
+}
